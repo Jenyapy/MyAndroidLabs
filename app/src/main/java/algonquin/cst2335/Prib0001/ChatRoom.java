274cce507fbc017feff20d9b9d1b2666fa8a92b9
Jenyapy <Jenya.py@gmail.com>
2021-07-09 23:16:07 -0400

    Finished Database Insertions

--- app/src/main/java/algonquin/cst2335/Prib0001/ChatRoom.java
+++ app/src/main/java/algonquin/cst2335/Prib0001/ChatRoom.java
@@ -1,12 +1,17 @@
 package algonquin.cst2335.Prib0001;
+
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -16,113 +21,185 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.Locale;
 
+import com.google.android.material.snackbar.Snackbar;
+
+
+
+
 public class ChatRoom extends AppCompatActivity {
+
+
     RecyclerView chatList;
+
+    MyChatAdapter adt;
+    ArrayList<MyChatAdapter.ChatMessage> messages = new ArrayList<>();
+    SQLiteDatabase db;
+    private Button send;
+    private Button receive;
+    String message;
+    int sendOrReceive;
+    Date timeSent;
+    public int getMessage;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ArrayList<ChatMessage> messages = new ArrayList<>();
         setContentView(R.layout.chatlayout);
-        chatList = findViewById(R.id.myRecycler);
-        MyChatAdapter adt = new MyChatAdapter();
-        chatList.setAdapter(new ChatAdapter() );
-chatList.setAdapter(adt);
-    chatList.setLayoutManager(new LinearLayoutManager(this));
+
+        EditText editText = findViewById(R.id.editTextTextPersonName);
+        MyOpenHelper opener = new MyOpenHelper(this);
+        SQLiteDatabase db = opener.getWritableDatabase();
+        Cursor results = db.rawQuery("SELECT * FROM " + MyOpenHelper.TABLE_NAME + ";", null);
 
 
-MyOpenHelper opener = new MyOpenHelper();
+        send = findViewById(R.id.sendbutton);
+        receive = findViewById(R.id.receivebutton);
+        int colId = results.getColumnIndex("_id");
+        int colMessage = results.getColumnIndex(MyOpenHelper.col_message);
+        int colSend = results.getColumnIndex(MyOpenHelper.col_send_receive);
+        int colTime = results.getColumnIndex(MyOpenHelper.col_time_sent);
+
+        editText.setText("");
+        ArrayList<MyChatAdapter.ChatMessage> messages = new ArrayList<>();
+        chatList = findViewById(R.id.myrecycler);
+        adt = new MyChatAdapter();
+        chatList.setAdapter(adt);
+        chatList.setLayoutManager(new LinearLayoutManager(this));
+        send.setOnClickListener(click ->{
+           ChatMessage cm = new ChatMessage(editText.getText().toString(),1,new Date());
+            ContentValues newRow = new ContentValues();
+            newRow.put(MyOpenHelper.col_message, message);
+            newRow.put(MyOpenHelper.col_send_receive, sendOrReceive);
+            newRow.put(MyOpenHelper.col_time_sent, String.valueOf(timeSent));
+           long id =  db.insert(MyOpenHelper.TABLE_NAME,MyOpenHelper.col_message,newRow);
+
+
+adt.notifyItemInserted(messages.size()-1);
+        });
+
 
 
 
     }
 
-    private class MyRowViews extends RecyclerView.ViewHolder{
-// class will hold widgets on row
 
+
+
+
+    private class MyRowViews extends RecyclerView.ViewHolder{
+
+        AlertDialog.Builder builder = new AlertDialog.Builder( ChatRoom.this );
         TextView messageText;
         TextView timeText;
+        int position = -1;
         public MyRowViews( View itemView) {
             super(itemView);
 
-            itemView.setOnClickListener(click ->{
-                AlertDialog.Builder builder = new AlertDialog.Builder( ChatRoom.this );
-                builder.setTitle("Question:");
-                builder.setPositiveButton("No", (dialog, cl) -> { });
-                builder.setNegativeButton("Yes", (dialog, cl) -> { });
-                message.remove(position);
-                adt.notifyItemRemoved(position);
-
-
+            itemView.setOnClickListener(click -> {
                 messages.remove(position);
-                adt.notifyItemRemoved(setPosition);
+                builder.setMessage("Do you want to delete the message:" + messageText.getText());
+                builder.setTitle("Question:");
+                builder.setNegativeButton("Yes", (dialog, cl) -> {
+                });
+                builder.setNegativeButton("No", (dialog, cl) -> {
+                });
+                position = getAbsoluteAdapterPosition();
+               MyChatAdapter.ChatMessage removedMessage = messages.get(position);
+                messages.remove(position);
+                adt.notifyItemRemoved(position);
+                Snackbar.make(messageText, "You deleted message #" + position, Snackbar.LENGTH_LONG)
+                        .setAction("undo", clk -> {
+                            messages.add(position, removedMessage);
+                            adt.notifyItemInserted(position);});
 
+
+      //  db.insert(MyOpenHelper.TABLE_NAME, MyOpenHelper.col_message,newRow);
 
             });
-            .create().show();
-            messageText = itemView.findViewById(R.id.message);
+
+            builder.create().show();
+
+            messageText = itemView.findViewById(R.id.receivemessage);
             timeText = itemView.findViewById(R.id.time);
         }
         public void setPosition(int p) {position = p;}
     }
 
-
-   private class ChatAdapter extends RecyclerView.Adapter{
-
+    private class MyChatAdapter extends RecyclerView.Adapter<MyRowViews>{
 
 
-       @Override
-       public RecyclerView.ViewHolder onCreateViewHolder( ViewGroup parent, int viewType) {
 
+        @Override
+        public ChatRoom.MyRowViews onCreateViewHolder( ViewGroup parent, int viewType) {
            LayoutInflater inflater = getLayoutInflater();
-           int layoutID;
-           if(ViewType == 1)
-               layoutID = R.layout.sent_message;
-           else
-               layoutID = R.layout.receive_message;
-           View loadedRow = inflater.inflate(layoutID, parent,false);
+           View loadedRow = inflater.inflate(
+                   viewType == 1? R.layout.sent_message: R.layout.receive_message, parent, false);
 
-           MyRowViews initRow = new MyRowViews(loadedRow);
+            int layoutID;
+            if(viewType ==1)
+                layoutID = R.layout.sent_message;
+            else
+                layoutID = R.layout.receive_message;
 
-           return initRow;
-       }
+            ChatRoom.MyRowViews initRow = new ChatRoom.MyRowViews(loadedRow);
+            return initRow;
+        }
 
-       @Override
-       public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
-           MyRowViews thisRowLayout = (MyRowViews) holder;
-            ((MyRowViews) holder).messageText.setText(messages.get(position).getMessage());
-            ((MyRowViews) holder).timeText.setText(messages.get(position).getTimeSent());
-            ((MyRowViews) holder).setPosition(position);
+        @Override
+        public void onBindViewHolder( MyRowViews holder, int position) {
+            holder.messageText.setText(messages.get(position).getMessage);
+            holder.timeText.setText((CharSequence) messages.get(position).getTimeSent());
+            holder.setPosition(position);
+        }
 
-
-       }
-
-       @Override
-       public int getItemCount() {
-           return 10;// how many items to show
-       }
-   }
+        @Override
+        public int getItemCount() {
+            return 0;
+        }
 
 
-
-
-
-   private class ChatMessage
-    {
-    String message;
-    int sendOrReceive;
-    Date timeSent;
-
-
-        public ChatMessage(String message, int sendOrReceive, Date timeSent) {
-            this.message = message;
-            this.sendOrReceive = sendOrReceive;
-            this.timeSent = timeSent;
-
-            SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault());
-
+        private class ChatMessage
+        {
+            String message;
+            int sendOrReceive;
+            Date timeSent;
+            public int getMessage;
+            long id;
             String currentDateandTime = sdf.format(new Date());
 
+    public void setid(long l) {id = l;}
+    public long getId() {return id;}
 
+
+
+
+
+
+            ContentValues newRow = new ContentValues();
+            public ChatMessage(String message, int sendOrReceive, Date timeSent) {
+                this.message = message;
+                this.sendOrReceive = sendOrReceive;
+                this.timeSent = timeSent;
+
+            }
+
+            public String getMessage() {
+
+                return message;
+            }
+
+            public int getSendOrReceive() {
+                return sendOrReceive;
+            }
+
+            public String getTimeSent() {
+
+                SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault());
+                currentDateandTime = sdf.format(timeSent);
+                return currentDateandTime;
+
+                }
+            }
         }
     }
-}
+
--- app/src/main/java/algonquin/cst2335/Prib0001/MyOpenHelper.java
+++ app/src/main/java/algonquin/cst2335/Prib0001/MyOpenHelper.java
@@ -12,11 +12,17 @@ public class MyOpenHelper extends SQLiteOpenHelper {
     public static final int version = 1;
     public static final String TABLE_NAME = "Messages";
     public static final String col_message = "Message";
+
     public static final String col_send_receive = "SendOrReceive";
     public static final String col_time_sent = "TimeSent";
+
+
+
     public MyOpenHelper(Context context) {
         super(context, name, null, version);
     }
+
+
 
     @Override
     public void onCreate(SQLiteDatabase db) {
